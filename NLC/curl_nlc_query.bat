9aff20d576c0fd1fbe09c30c3a0df136e776b877
Raghavendra Deshpande <ragdeshp@in.ibm.com>
2017-05-16 11:07:26 +0530

    NLC files

---
+++ NLC/curl_nlc_query.bat
@@ -0,0 +1,2 @@
+curl -G -u "userid":"passwd" "https://gateway.watsonplatform.net/natural-language-classifier/api/v1/classifiers/<class id>/classify" --data-urlencode "text=How freezing will it be today?"
+
